2d10557256d408a69a32a624cae1297a5ee74835
Anas <anasmv886@gmail.com>
2020-02-12 16:52:42 +0530

    Add files via upload
    printing values inside '{ }'  separate with comma ','

---
+++ print in array vlues  inside { , } .php
@@ -0,0 +1,24 @@
+<?php
+$nxt_bill = "{p2,410}{p1,510}";
+$nxt_bill1=ltrim(substr_replace($nxt_bill,"",-1),"{");
+
+$delimiter = array(" ","/","}{","{","}",);
+$replace = str_replace($delimiter, $delimiter[0], $nxt_bill1);
+$explode = explode($delimiter[0], $replace);
+
+echo "<pre>";
+$t=print_r($explode);
+echo "</pre>";
+
+echo "<br>";
+
+foreach ($explode as $line) {
+	$array1  = explode(",", $line);
+	foreach ($array1 as $line1) {
+        if($line1 != '')
+        {
+			echo $line1; echo '<br>';
+		}
+	}
+}
+?>
