1fcbdfc65b9be1c3951fe7208462a671c7b50bfa
Anas <anasmv886@gmail.com>
2020-03-19 15:44:42 +0530

    Add files via upload

---
+++ array encode as string.php
@@ -0,0 +1,16 @@
+<?php
+
+$orders  = array("name", "price", "qty");
+echo $enc=  encode_arr($orders);
+echo '<br>';
+echo '<pre>';
+print_r(decode_arr($enc));
+echo '</pre>';
+function encode_arr($data) {
+    return base64_encode(serialize($data));
+}
+
+function decode_arr($data) {
+    return unserialize(base64_decode($data));
+}
+?>
